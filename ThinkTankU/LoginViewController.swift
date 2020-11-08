f73f8f06cc85be3fdd8bd5607d151890c5be25f0
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-11-08 14:37:06 -0800

    Login and Signup View Controller
    Desgined the login and singup view controller and created View Controller classes for them.
    NEXT STEPS:
    - Set of Parse Backend for app to test functionality
    - Create transitions between the two screens. Signup button should take you to Signup Screen, and register button should take you back to the LogIn screen after creating the user.

---
+++ ThinkTankU/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  ThinkTankU
+//
+//  Created by Sivam Agarwalla on 11/7/20.
+//  Copyright © 2020 Sivam Agarwalla. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ThinkTankU/SignupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupViewController.swift
+//  ThinkTankU
+//
+//  Created by Sivam Agarwalla on 11/8/20.
+//  Copyright © 2020 Sivam Agarwalla. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
