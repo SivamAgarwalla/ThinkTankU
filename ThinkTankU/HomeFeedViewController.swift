b251018f4f36d21d4bfb8833d6d7213708a8374d
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-11-12 19:57:40 -0800

    Initial HomeScreen Post Listing Complete
    IMPORTANT : When the user Saves a post it should clear all the textFields and the safe button should segue back to the home screen.

--- ThinkTankU/HomeFeedViewController.swift
+++ ThinkTankU/HomeFeedViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class HomeFeedViewController: UIViewController {
-
+class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var postsTableView: UITableView!
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        postsTableView.delegate = self
+        postsTableView.dataSource = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.order(byDescending: "createdAt")
+        query.includeKey("author")
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.postsTableView.reloadData()
+            }
+            else {
+                print("Error: \(error?.localizedDescription ?? "There was an error loading the posts!")")
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = postsTableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let postUser = post["author"] as! PFUser
+        cell.username.text = postUser["username"] as? String
+        cell.userStartup.text = postUser["startup"] as? String
+        cell.postTitle.text = post["title"] as? String
+        cell.postDescription.text = post["description"] as? String
+        
+        let postImageFile = post["image"] as! PFFileObject
+        let urlString = postImageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.postImageView.af.setImage(withURL: url)
+        
+        return cell
     }
     
 
---
+++ ThinkTankU/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  ThinkTankU
+//
+//  Created by Sivam Agarwalla on 11/12/20.
+//  Copyright © 2020 Sivam Agarwalla. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var userStartup: UILabel!
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var postDescription: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
